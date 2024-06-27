226272fe121780ac01575e58ed8d2a8d3bc924bb
mohammad rtr <assd13@gmail.com>
2024-06-27 05:03:54 -0700

    compleate project

---
+++ app/app.js
@@ -0,0 +1,9 @@
+const li = document.getElementById("spanD").classList.toggle("span") ;
+
+const div = document.querySelector("#nanoD").classList.add("nano");
+
+const tag = document.getElementById("infoD").classList.toggle("info");
+
+const h1 = document.querySelector("#txtD").classList.add("txt") ;
+
+const lop = document.querySelector("#lopD").classList.add("lop");
